a1fe870c056e67c77c51214441671cdc412965e2
paularah <p.arah@alustudent.com>
2021-11-24 01:48:21 +0200

    fib tabulation

--- main.py
+++ main.py
@@ -19,4 +19,41 @@ def basicCanSum(n, m):
         hashmap[n-i] = True
 
 
-print(basicCanSum(7, [5, 3, 4, 7]))
+# print(basicCanSum(7, [5, 3, 4, 7]))
+
+
+"""
+fib tabulation
+n = 6
+ 0  1  2  3  4  5  6
+[0, 1, 0, 0, 0, 0, 0]
+[0, 1, 1, 2, 3, 5, ]
+"""
+
+
+def fibTable(n):
+    table = [0 for _ in range(n+1)]
+    table[1] = 1
+    for index, val in enumerate(table):
+        if index + 1 <= n:
+            table[index + 1] += val
+        if index + 2 <= n:
+            table[index + 2] += val
+    print(table)
+    return table[n]
+
+
+print(fibTable(5))
+
+
+def search(nums, target):
+    middle = len(nums)//2
+    while middle >= 0:
+        if nums[middle] == target:
+            return middle
+        if nums[middle] < target:
+            nums = nums[middle:]
+        if nums[middle] > target:
+            nums = nums[:middle]
+        middle = len(nums)//2
+    return -1

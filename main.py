846f9fa2a7965d5475305171430418b350aa74e9
paularah <p.arah@alustudent.com>
2021-11-25 00:38:28 +0200

    grid traveller tabulation

--- main.py
+++ main.py
@@ -43,7 +43,20 @@ def fibTable(n):
     return table[n]
 
 
-print(fibTable(5))
+def gridTravellerTable(m, n):
+    table = [[0 for _ in range(n+1)] for _ in range(m + 1)]
+    table[1][1] = 1
+    for i in range(m+1):
+        for j in range(n+1):
+            current = table[i][j]
+            if j + 1 <= n:
+                table[i][j + 1] += current
+            if i + 1 <= m:
+                table[i+1][j] += current
+    return table[m][n]
+
+
+print(gridTravellerTable(2, 3))
 
 
 def search(nums, target):
